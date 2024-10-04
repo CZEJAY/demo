995ca6816b80e2ff32b432a072f57379079fd726
Joshardals <joshuabamidele219@gmail.com>
2024-10-04 05:23:31 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,9 @@
-// import { Waitlist } from "@/components/Landing Page/Waitlist";
+import { HeroSection } from "@/components/Landing Page/HeroSection";
 
 export default function Home() {
   return (
     <main>
-      <section className="flex items-center justify-center h-screen p-4 bg-black text-silverGray">
-        <div className="max-content text-center">
-          <div className="space-y-5 mb-5 lg:mb-10">
-            <span className="text-xl tracking-wide">
-              Experience a new way to share your stories and ideas.
-            </span>
-
-            <div className="max-w-4xl">
-              <h3 className=" leading-tight text-[4rem] max-lg:text-[2.5rem] text-white font-bold max-lg:font-medium">
-                <span className="bg-gradient-to-r from-deepTeal to-skyAqua bg-clip-text text-transparent">
-                  AI-Powered
-                </span>{" "}
-                Content Creation Platform
-              </h3>
-            </div>
-          </div>
-
-          <p className=" md:max-w-2xl text-pretty mx-auto mb-12 hidden">
-            {/* Create beautiful presentations, pitch decks, resumes, websites, and
-            documents. <br /> No design, writing, or coding skills needed.{" "}
-            <br />
-            Bring your ideas to life like never before. */}
-            Beautiful presentations, documents, and websites. No design or
-            coding skills required. <br /> Bring your ideas to life
-            effortlessly.
-          </p>
-
-          <button className="px-6 py-3 bg-gradient-to-r from-deepTeal to-skyAqua text-white rounded-lg font-medium">
-            Sign Up Now
-          </button>
-
-          {/* <Waitlist /> */}
-        </div>
-      </section>
+      <HeroSection />
     </main>
   );
 }
---
+++ components/Landing Page/HeroSection.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { useEffect, useState } from "react";
+import Typewriter from "typewriter-effect";
+
+// import { Waitlist } from "@/components/Landing Page/Waitlist";
+
+export function HeroSection() {
+  const words = [
+    "Presentations",
+    "Pitch Decks",
+    "Resumes",
+    "Websites",
+    "Documents",
+  ];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 1000); // Change word every 1 second
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <section className="flex items-center justify-center h-screen p-4 bg-black text-silverGray">
+      <div className="max-content text-center">
+        <div className="space-y-5 mb-5 lg:mb-10">
+          <span className="text-xl tracking-wide">
+            Experience a new way to share your stories and ideas.
+          </span>
+
+          <div className="max-w-4xl transition-all delay-500 ease-linear">
+            <h1 className=" leading-tight text-[4rem] max-lg:text-[2.5rem] text-white font-bold max-lg:font-medium inline-block">
+              AI-Powered{" "}
+              <span className="text-deepTeal inline-block">
+                {" "}
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Content",
+                      "Presentations",
+                      "Pitch Decks",
+                      "Resumes",
+                      "Websites",
+                      "Documents",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 100, // Adjust the speed of deletion
+                  }}
+                />
+              </span>{" "}
+              Creation Platform
+            </h1>
+          </div>
+        </div>
+
+        <p className=" md:max-w-2xl text-pretty mx-auto mb-12 hidden">
+          {/* Create beautiful presentations, pitch decks, resumes, websites, and
+        documents. <br /> No design, writing, or coding skills needed.{" "}
+        <br />
+        Bring your ideas to life like never before. */}
+          Beautiful presentations, documents, and websites. No design or coding
+          skills required. <br /> Bring your ideas to life effortlessly.
+        </p>
+
+        <button className="px-6 py-3 bg-gradient-to-r from-deepTeal to-skyAqua text-white rounded-lg font-medium">
+          Sign Up Now
+        </button>
+
+        {/* <Waitlist /> */}
+      </div>
+    </section>
+  );
+}
