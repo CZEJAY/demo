c0586555c3c509b3e893bdd4d7bc9faad5b5ef8a
Joshardals <joshuabamidele219@gmail.com>
2024-10-04 02:53:42 +0100

    initial commit

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 p-4 bg-white border-b border-b-red-charcoal">
       <div className="max-content flex justify-between items-center">
-        <span className="text-2xl font-bold text-black">Atoovis Create</span>
+        <span className="text-2xl font-bold text-black">Patexa</span>
 
         <div className="flex items-center space-x-8">
           <SlGlobe className="cursor-pointer" />
