b9f4e1c2a0b0a671df3046a5179d6eb37e5ee3b2
Caleb <calebjimmy67@gmail.com>
2024-12-13 00:57:34 +0100

    valid export

--- app/doc/route.ts
+++ app/doc/route.ts
@@ -1,20 +1,20 @@
-import { customAlphabet } from "nanoid";
+import { customAlphabet } from 'nanoid'
 
-export const dynamic = "auto";
-export const dynaminParams = true;
-export const runtime = "edge";
+export const dynamic = 'auto'
+export const dynamicParams = true
+export const runtime = 'edge'
 
 const getNanoId = () => {
-  const nanoid = customAlphabet("6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz", 10);
-  return nanoid();
-};
+  const nanoid = customAlphabet('6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz', 10)
+  return nanoid()
+}
 
 export async function GET(): Promise<Response> {
   return new Response(null, {
     status: 307, // 308 for permanent redirect, 307 for temporary redirects
     headers: {
       Location: `/doc/untitled-${getNanoId()}`,
-      "cache-control": "no-store, max-age=0",
+      'cache-control': 'no-store, max-age=0',
     },
-  });
+  })
 }
